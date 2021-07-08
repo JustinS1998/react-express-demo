417f67b91f9c91e97e2c55372f91558db646eebe
Justin Seitz <justinseitz2016@gmail.com>
2021-07-08 14:57:57 -0700

    can now send data to mongodb atlas

--- server/index.js
+++ server/index.js
@@ -15,20 +15,19 @@ app.get("/api", (req, res) => {
     res.json({message: "Hello from server"});
 });
 
+const MongoClient = require('mongodb').MongoClient;
+const username = 'admin';
+const password = '8uEUQySVIJkTNB9T';
+const connectionString = `mongodb+srv://${username}:${password}@cluster0.drkd2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 app.post("/message", (req, res) => {
     console.log('Got a POST request');
     console.log(req.body);
+    MongoClient.connect(connectionString, {useUnifiedTopology:true})
+    .then((client) => {
+        console.log('Connected to database');
+        const db = client.db('test');
+        const testCollection = db.collection('testCollection');
+        testCollection.insertOne(req.body);
+    })
+    .catch((error) => console.error(error));
 })
-
-// let MongoClient = require('mongodb').MongoClient;
-// const username = 'admin';
-// const password = '8uEUQySVIJkTNB9T';
-// const connectionString = `mongodb+srv://${username}:${password}@cluster0.drkd2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-// MongoClient.connect(connectionString, {useUnifiedTopology:true})
-//     .then((client) => {
-//         console.log('Connected to database');
-//         const db = client.db('test');
-//         const testCollection = db.collection('testCollection');
-//         testCollection.insertOne({message:'test'});
-//     })
-//     .catch((error) => console.error(error));
